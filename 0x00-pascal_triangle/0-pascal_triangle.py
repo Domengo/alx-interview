9b682f58556c3674670f09e5eac29ab192f8d297
Domengo <dominicsengo@gmail.com>
2023-04-24 02:32:09 +0300

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    # Create an empty list to hold each row of the triangle
+    triangle = []
+    for i in range(n):
+        # Create a new list for each row
+        row = []
+        for j in range(i+1):
+            # Calculate the value of each element in the row
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                # Each element is the sum of the two elements above it in the previous row
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        # Append the row to the triangle list
+        triangle.append(row)
+    # Print the triangle
+    return triangle
