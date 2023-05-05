81a4228f7969cab6c786cd9eecf4e11848e07cce
Domengo <dominicsengo@gmail.com>
2023-05-05 04:11:51 +0300

    min op

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,40 +4,40 @@
 
 def minOperations(n):
     ''''minimum operations'''
-    if n < 2:
+    if n == 1:
         return 0
 
-    count = 0
-    op = 2
+    operations = 0
+    h_count = 1
+    clipboard = 1
 
-    while n > 1:
-        if not n % op:
-            n //= op
-            count += op
+    while h_count < n:
+        if n % h_count == 0:
+            clipboard = h_count
+        h_count += clipboard
+        operations += 1
 
-        else:
-            if op == 2:
-                op += 1
-            else:
-                op += 2
-
-    return count
-
+    if h_count == n:
+        return operations
+    else:
+        return 0
 # def minOperations(n):
-#     if n == 1:
+#     
+#     if n < 2:
 #         return 0
 
-#     operations = 0
-#     h_count = 1
-#     clipboard = 1
+#     count = 0
+#     op = 2
 
-#     while h_count < n:
-#         if n % h_count == 0:
-#             clipboard = h_count
-#         h_count += clipboard
-#         operations += 1
+#     while n > 1:
+#         if not n % op:
+#             n //= op
+#             count += op
 
-#     if h_count == n:
-#         return operations
-#     else:
-#         return 0
+#         else:
+#             if op == 2:
+#                 op += 1
+#             else:
+#                 op += 2
+
+#     return count
