fe586072c4b48b055db5ec8cde4752a4839560c1
Domengo <dominicsengo@gmail.com>
2023-05-18 13:23:16 +0300

    validate

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""a method that determines if a given data set
-represents a valid UTF-8 encoding."""
+"""
+UTF-8 Validation
+"""
 
 
-def validUTF8(data):
-    """Return: True if data is a valid UTF-8 encoding, else return False"""
+def validUTF8(data) -> bool:
+    """
+    Returns True if data is a valid UTF-8 encoding, else return False
+    :param data:
+    :return:
+    """
     num_bytes = 0
     for byte in data:
-        # If num_bytes is 0, we are starting a new character
         mask = 1 << 7
         if not num_bytes:
             while byte & mask:
@@ -18,10 +22,7 @@ def validUTF8(data):
             if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            # Check if the byte is a valid continuation byte
             if byte >> 6 != 0b10:
                 return False
-            num_bytes -= 1
-
-    # If there are remaining bytes, it's an invalid encoding
+        num_bytes -= 1
     return num_bytes == 0
