5ba0e28a512f58e7014f1fd3f9d5148a32b513b8
Domengo <dominicsengo@mail.com>
2023-06-09 13:39:53 +0300

    fixed pep8 long line

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,15 +5,30 @@ Test 0x07 - Rotate 2D Matrix
 
 
 def rotate_2d_matrix(matrix):
-    """_summary_
+    """
+    Rotates a 2D matrix in place.
 
     Args:
-        matrix (_type_): _description_
+        matrix: The 2D matrix to rotate.
+
+    Returns:
+        None
     """
+    # for i in range(len(matrix) // 2):
+    #     for j in range(i, len(matrix) - i - 1):
+    #         matrix[i][j], matrix[j][len(matrix) - i - 1], \
+    #         matrix[len(matrix) - i - 1][len(matrix) - j - 1], \
+    #         matrix[len(matrix) - j - 1][i] \
+    #         = matrix[len(matrix) - j - 1][i], \
+    #         matrix[i][j], matrix[j][len(matrix) - i - 1], \
+    #         matrix[len(matrix) - i - 1][len(matrix) - j - 1]
+
     for i in range(len(matrix) // 2):
-        for j in range(i, len(matrix) - i - 1):
-            matrix[i][j], matrix[j][len(matrix) - i - 1], \
-            matrix[len(matrix) - i - 1][len(matrix) - j - 1], \
-            matrix[len(matrix) - j - 1][i] = matrix[len(matrix) - j - 1][i], \
-            matrix[i][j], matrix[j][len(matrix) - i - 1], \
-            matrix[len(matrix) - i - 1][len(matrix) - j - 1]
+        for j in range(i, len(matrix[0]) - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[len(matrix) - 1 - j][i]
+            matrix[len(matrix) - 1 - j][i] \
+                = matrix[len(matrix) - 1 - i][len(matrix) - 1 - j]
+            matrix[len(matrix) - 1 - i][len(matrix) - 1 - j] \
+                = matrix[j][len(matrix) - 1 - i]
+            matrix[j][len(matrix) - 1 - i] = temp
