374fac69331ed67dad460bcb9a56e970e0942bf4
Domengo <dominicsengo@mail.com>
2023-06-10 16:18:45 +0300

    update

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -34,7 +34,7 @@ def rotate_2d_matrix(matrix):
     #         matrix[j][len(matrix) - 1 - i] = temp
     n = len(matrix)
 
-    # Transpose the matrix
+    '''Transpose the matrix'''
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
