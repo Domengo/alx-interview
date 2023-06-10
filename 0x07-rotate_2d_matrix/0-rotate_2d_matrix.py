db84d78b1615ad37b1705dbd6e267aefb0d1aa60
Domengo <dominicsengo@mail.com>
2023-06-10 16:15:24 +0300

    update

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -23,12 +23,22 @@ def rotate_2d_matrix(matrix):
     #         matrix[i][j], matrix[j][len(matrix) - i - 1], \
     #         matrix[len(matrix) - i - 1][len(matrix) - j - 1]
 
-    for i in range(len(matrix) // 2):
-        for j in range(i, len(matrix[0]) - i - 1):
-            temp = matrix[i][j]
-            matrix[i][j] = matrix[len(matrix) - 1 - j][i]
-            matrix[len(matrix) - 1 - j][i] \
-                = matrix[len(matrix) - 1 - i][len(matrix) - 1 - j]
-            matrix[len(matrix) - 1 - i][len(matrix) - 1 - j] \
-                = matrix[j][len(matrix) - 1 - i]
-            matrix[j][len(matrix) - 1 - i] = temp
+    # for i in range(len(matrix) // 2):
+    #     for j in range(i, len(matrix[0]) - i - 1):
+    #         temp = matrix[i][j]
+    #         matrix[i][j] = matrix[len(matrix) - 1 - j][i]
+    #         matrix[len(matrix) - 1 - j][i] \
+    #             = matrix[len(matrix) - 1 - i][len(matrix) - 1 - j]
+    #         matrix[len(matrix) - 1 - i][len(matrix) - 1 - j] \
+    #             = matrix[j][len(matrix) - 1 - i]
+    #         matrix[j][len(matrix) - 1 - i] = temp
+    n = len(matrix)
+
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
