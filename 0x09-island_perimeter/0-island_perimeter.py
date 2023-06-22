45a711dfba58244eb7e270c997b8dbb7d18962a4
Domengo <dominicsengo@gmail.com>
2023-06-22 17:20:32 +0300

    island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,33 +3,33 @@
 
 
 def island_perimeter(grid):
-	"""_summary_
+    """_summary_
 
-	Args:
-		grid (_type_): _description_
+    Args:
+        grid (_type_): _description_
 
-	Returns:
-	_type_: _description_
-	"""
+    Returns:
+    _type_: _description_
+    """
 
-	if not grid:
-		return 0
+    if not grid:
+        return 0
 
-	rows = len(grid)
-	cols = len(grid[0])
-	perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
 
-	for row in range(rows):
-		for col in range(cols):
-			if grid[row][col] == 1:
-				perimeter += 4
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
 
-				# Check left neighbor
-				if col > 0 and grid[row][col - 1] == 1:
-					perimeter -= 2
+                # Check left neighbor
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
 
                 # Check top neighbor
-				if row > 0 and grid[row - 1][col] == 1:
-					perimeter -= 2
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
 
-	return perimeter
+    return perimeter
