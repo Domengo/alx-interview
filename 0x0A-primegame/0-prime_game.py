b8d9c450e707eb3c44265d29c8815ea2a848294b
Domengo <dominicsengo@gmail.com>
2023-06-29 14:25:23 +0300

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,7 +2,41 @@
 """_summary_ - find the most wins
 """
 
+def isWinner(x, nums):
+    """_summary_
 
+    Args:
+        x (_type_): _description_
+        nums (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        primes = [True] * (n+1)
+        primes[0] = primes[1] = False
+
+        for i in range(2, int(n**0.5) + 1):
+            if primes[i]:
+                for j in range(i*i, n+1, i):
+                    primes[j] = False
+
+        # Determine the winner for the current round
+        if primes.count(True) % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    # Determine the overall winner
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
 # def isWinner(x, nums):
 #     """_summary_ - find the most wins
 #     Args:
@@ -32,29 +66,3 @@
 #     if p1 * 2 > len(nums):
 #         return "Maria"
 #     return "Ben"
-def isWinner(x, nums):
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        primes = [True] * (n+1)
-        primes[0] = primes[1] = False
-
-        for i in range(2, int(n**0.5) + 1):
-            if primes[i]:
-                for j in range(i*i, n+1, i):
-                    primes[j] = False
-
-        # Determine the winner for the current round
-        if primes.count(True) % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
-
-    # Determine the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
