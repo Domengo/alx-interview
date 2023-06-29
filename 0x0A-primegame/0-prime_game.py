5e6ef3c13e33fb8f3d8b4b3d45703f57308c022d
Domengo <dominicsengo@gmail.com>
2023-06-29 14:28:41 +0300

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,43 +1,47 @@
 #!/usr/bin/python3
-"""_summary_ - find the most wins
+"""
+Define isWineer function, a solution to the Prime Game problem
 """
 
 
-def isWinner(x, nums):
-    """_summary_
-
-    Args:
-        x (_type_): _description_
-        nums (_type_): _description_
-
-    Returns:
-        _type_: _description_
+def primes(n):
+    """Return list of prime numbers between 1 and n inclusive
+        Args:
+        n (int): upper boundary of range. lower boundary is always 1
     """
-    maria_wins = 0
-    ben_wins = 0
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
 
-    for n in nums:
-        primes = [True] * (n+1)
-        primes[0] = primes[1] = False
 
-        for i in range(2, int(n**0.5) + 1):
-            if primes[i]:
-                for j in range(i*i, n+1, i):
-                    primes[j] = False
-
-        # Determine the winner for the current round
-        if primes.count(True) % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
-
-    # Determine the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+def isWinner(x, nums):
+    """
+    Determines winner of Prime Game
+    Args:
+        x (int): no. of rounds of game
+        nums (int): upper limit of range for each round
+    Return:
+        Name of winner (Maria or Ben) or None if winner cannot be found
+    """
+    if x is None or nums is None or x == 0 or nums == []:
         return None
+    Maria = Ben = 0
+    for i in range(x):
+        prime = primes(nums[i])
+        if len(prime) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
 # def isWinner(x, nums):
 #     """_summary_ - find the most wins
 #     Args:
