6f919ce489edb405e13f63dca3a5974da48dbb86
Domengo <dominicsengo@gmail.com>
2023-06-29 14:16:08 +0300

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,32 +3,45 @@
 """
 
 
+# def isWinner(x, nums):
+#     """_summary_ - find the most wins
+#     Args:
+#         x ([type]): [description]
+#         nums ([type]): [description]
+#     """
+#     if not nums or x < 1:
+#         return None
+#     n = max(nums)
+#     nums.sort()
+#     m = [False for i in range(n + 1)]
+#     for i in range(2, n + 1):
+#         if not m[i]:
+#             for j in range(i, n + 1, i):
+#                 m[j] = True
+#     m[0] = m[1] = False
+#     c = 0
+#     for i in range(len(m)):
+#         if m[i]:
+#             c += 1
+#         m[i] = c
+#     p1 = 0
+#     for n in nums:
+#         p1 += m[n] % 2 == 1
+#     if p1 * 2 == len(nums):
+#         return None
+#     if p1 * 2 > len(nums):
+#         return "Maria"
+#     return "Ben"
+
 def isWinner(x, nums):
-    """_summary_ - find the most wins
-    Args:
-        x ([type]): [description]
-        nums ([type]): [description]
-    """
-    if not nums or x < 1:
-        return None
-    n = max(nums)
-    nums.sort()
-    m = [False for i in range(n + 1)]
-    for i in range(2, n + 1):
-        if not m[i]:
-            for j in range(i, n + 1, i):
-                m[j] = True
-    m[0] = m[1] = False
-    c = 0
-    for i in range(len(m)):
-        if m[i]:
-            c += 1
-        m[i] = c
-    p1 = 0
-    for n in nums:
-        p1 += m[n] % 2 == 1
-    if p1 * 2 == len(nums):
-        return None
-    if p1 * 2 > len(nums):
-        return "Maria"
-    return "Ben"
+    winners = []
+    for i in range(x):
+        if nums[0] == 1:
+            winners.append("Ben")
+        elif nums[0] == 2:
+            winners.append("Maria")
+        else:
+            winners.append("Maria")
+            nums = [i for i in nums if i % nums[0] != 0]
+    return max(set(winners), key=winners.count)
+
